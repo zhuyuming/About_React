372e7aeeb6a3ccae444bb687a557306846cf1b16
zhongxia245 <zhongxia245@sina.com>
2016-01-12 15:32:17 +0800

    1.开发环境使用 热部署的方式，那么发布的时候，如何直接引用JS就可以访问页面效果呢？ 修改 webpack.config.js 里面的 入口，注释掉 热部署的那一句代码即可

--- react_webpack/webpack.config.js
+++ react_webpack/webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 module.exports = {
     entry: [
-        'webpack/hot/only-dev-server',
+        'webpack/hot/only-dev-server',   //如果需要发布的时候，去掉这段热部署，然后再打包一下，便可以直接引用JS看效果
         "./js/app.js"
     ],
     output: {
