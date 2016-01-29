ec267a90b02dabc95b4b18fc7d544a1ca653bc74
zhongxia245 <zhongxia245@sina.com>
2016-01-30 01:25:25 +0800

    bind2Prop 刚开始的方法不可用用，重新修改了导入导出

--- redux-router-antd/components/index.js
+++ redux-router-antd/components/index.js
@@ -6,10 +6,19 @@
 import bind2Prop from '../redux/bind2Prop'
 
 // 导出组件(记得绑定上state 和 actions 到 props 上)
-export {default as bind2Prop(Header)} from './header';
-export {default as bind2Prop(Footer)} from './footer';
-export {default as bind2Prop(Main)} from './main';
-export {default as bind2Prop(Welcome)} from './welcome';
-export {default as bind2Prop(Table)} from './table';
-export {default as bind2Prop(FormDemo)} from './form';
-export {default as bind2Prop(ErrorPage)} from './error';
+import pre_Header from './header';
+import pre_Footer from './footer';
+import pre_Main from './main';
+import pre_Welcome from './welcome';
+import pre_Table from './table';
+import pre_FormDemo from './form';
+import pre_ErrorPage from './error';
+
+// 导出组件(记得绑定上state 和 actions 到 props 上)
+export const Header = bind2Prop(pre_Header) 
+export const Footer = bind2Prop(pre_Footer) 
+export const Main = bind2Prop(pre_Main) 
+export const Welcome = bind2Prop(pre_Welcome) 
+export const Table = bind2Prop(pre_Table) 
+export const FormDemo = bind2Prop(pre_FormDemo) 
+export const ErrorPage = bind2Prop(pre_ErrorPage) 
